1f1ae35e84c351b2255e837dad5068126af7643c
Jean McLeod <abricsltd@gmail.com>
2022-10-01 00:11:28 +0100

    add after element action

--- Assets/js/DaD.js
+++ Assets/js/DaD.js
@@ -15,15 +15,23 @@ containers.forEach(container => {
         e.preventDefault()
         const afterElement = getDragAfterElement(container, e.clientY)
         const draggable = document.querySelector('.dragging')
+        if (afterElement == null) {
         container.appendChild(draggable)
+        }
     })
 })
 
 function getDragAfterElement(container, y){
  const draggableElements = [... container.querySelectorAll('.draggable:not(.dragging)')]
 
- draggableElements.reduce((closest, child) => {
-const box = child.getBoundingClientRect()
-console.log(box)
- }, {offset: Number.POSITIVE_INFININTY})
+ return draggableElements.reduce((closest, child) => {
+  const box = child.getBoundingClientRect()
+  const offset = y - box.top - box.height / 2
+  
+  if (offset < 0 && offset > closest.offset) {
+    return { offset: offset, element: child}    
+  } else {
+    return closest
+  }
+ }, {offset: Number.NEGATIVE_INFINITY}).element
 }
